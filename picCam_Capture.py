30c4ae234ac78824fb713b3d6d8d6a47f46a1f17
taiwc <wenchih4439@gmail.com>
2017-04-26 11:22:45 +0800

    picCam Client Server

--- picCam_Capture.py
+++ picCam_Capture.py
@@ -10,24 +10,95 @@ import sys
 import io
 import picamera
 import cv2
+#import msvcrt as m
 
-with picamera.PiCamera() as camera:
-    #camera.resolution=(640,480)
-    camera.start_preview()
-    print("Press Key to Save")
-    #time.sleep(20)
-    i=0
-    while True:
-        k=cv2.waitKey(500)
-        if k==27: #ESC key to stop
-            break
-        else:
+#def wait():
+#    m.getch()
+
+def wait():
+  try:
+    #no=sys.stdin.fileno()
+    #print "fileno ", no
+    c = sys.stdin.read(1)
+    #print "Got character", c #repr(c)
+  except IOError: pass
+  return c
+
+def socket_wait_id():
+  # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+  # all interfaces)
+  server_socket = socket.socket()
+  server_socket.bind(('0.0.0.0', 8000)) 
+  server_socket.listen(0)
+  server_connections = server_socket.accept()
+  connection=server_connections[0].makefile('rb')
+  ID=0
+  try:
+    rd=connection.read(struct.calcsize('<L'))
+    ID = struct.unpack('<L',rd)[0] 
+  finally:
+    connection.close()
+    server_socket.close()
+  return ID
+
+def socket_send_id(ip,ID):
+  client_socket = socket.socket()
+  client_socket.connect((ip, 8000))
+  connection = client_socket.makefile('wb')
+  try:
+    connection.write(struct.pack('<L', ID))
+  finally:
+    connection.close()
+    client_socket.close()
+    
+def proc_cam(camera,k,ii):
+        ret=1
+        i=ii
+        if k==ord('q'): #q key to stop
+          print("quit")
+          ret=0
+        elif k==-1:
+          ret=-1
+        elif k==ord('g'):
           filename="/home/pi/Python/image%02d.jpg" % i
           camera.capture(filename)
           print(filename)
+          img=cv2.imread(filename)
+          cv2.imshow(filename,img)
           i=i+1
-          if i>=20:
-              break
-          else:
-              time.sleep(1)
+        elif k==ord('s'):
+          print("stop_preview")
+          camera.stop_preview()
+        elif k==ord('v'):
+          print("start_preview")
+          camera.start_preview()
+        elif k==ord('6'):
+          print("640x480")
+          camera.resolution=(640,480)
+        elif k==ord('3'):
+          print("320x240")
+          camera.resolution=(320,240) 
+        else:
+          ret=1
+        return ret,i
+    
+with picamera.PiCamera() as camera:
+    camera.resolution=(320,240)
+    camera.start_preview()
+    print("Press Key g to Save")
+    #time.sleep(20)
+    i=0
+    while True:
+        #k=cv2.waitKey(500) & 0xff
+        #k=m.getch()
+        k=wait()
+        print(k)
+        ret,i=proc_cam(camera,ord(k),i)
+        if(ret==00):
+          break
+        else:
+          continue
+        
     camera.stop_preview()
+
+cv2.destroyAllWindows()
